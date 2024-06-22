ebe64600b8c5a9784e2628d7ec7532975ac426c9
samurai1106 <yassine2006hachimi@gmail.com>
2024-06-22 19:54:52 +0200

    11

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// ========================== Changing navbar color on scroll ==========================
+// ========================== Changing navbar opacity on scroll ==========================
 
 const navbar = document.querySelector("nav");
 
@@ -18,41 +18,6 @@ if (window.scrollY >= 100) {
 }
 });
 
-// ========================== Sections observing to change navbar links decoration ==========================
-
-const Sections = document.querySelectorAll(".main-section");
-const NavLinks = document.querySelectorAll(".navLinks");
-
-NavLinks.forEach((link) => {
-    link.addEventListener("click", (ev) => {
-        NavLinks.forEach((link) => {
-            link.classList.remove("active");
-        })
-        link.classList.add("active")
-    });
-});
-
-const SectionsOptions = {
-    threshold: 0.5,
-};
-
-const SectionsObserver = new IntersectionObserver((entries, SectionsObserver) => {
-entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-        return;
-    } else {
-        NavLinks.forEach((link) => {
-            link.classList.remove("active");
-        });
-    document.querySelector(`.${entry.target.id}`).classList.add("active");
-    }
-});
-}, SectionsOptions);
-
-Sections.forEach((Section) => {
-SectionsObserver.observe(Section);
-});
-
 // ========================== Responsive Menu ==========================
 
 const MenuBtn = document.querySelector("#menu-btn");
@@ -69,13 +34,17 @@ MenuLinks.forEach((Link) => {
     });
 });
 
-// ========================== Landing section heading animation (ISICOD) ==========================
-const animatedHeading = document.querySelector(".name h1");
+// ========================== Scroll to top button ==========================
+const scrollUpBtn = document.querySelector(".scroll-up");
 
-window.addEventListener("load", () => {
-    animatedHeading.classList.add("animate");
+window.addEventListener("scroll", () => {
+if (window.scrollY > 500) {
+    scrollUpBtn.classList.add("show-up-btn");
+} else {
+    scrollUpBtn.classList.remove("show-up-btn");
+}
 });
 
-setInterval(() => {
-    animatedHeading.classList.toggle("animate");
-}, 2800)
+scrollUpBtn.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+});
---
+++ js/observers.js
@@ -0,0 +1,136 @@
+// ========================== Sections observing to change navbar links decoration ==========================
+
+const Sections = document.querySelectorAll(".main-section");
+const NavLinks = document.querySelectorAll(".navLinks");
+
+const SectionsOptions = {
+    threshold: 1,
+};
+
+const SectionsObserver = new IntersectionObserver((entries, SectionsObserver) => {
+entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+        return;
+    } else {
+        NavLinks.forEach((link) => {
+            link.classList.remove("active");
+        });
+    document.querySelector(`.${entry.target.id}`).classList.add("active");
+    }
+});
+}, SectionsOptions);
+
+Sections.forEach((Section) => {
+SectionsObserver.observe(Section);
+});
+
+// ========================== Fade effect to element while scrolling ==========================
+window.addEventListener("load", () => {
+    // ========================== Fade top ==========================
+const fadersTop = document.querySelectorAll(".fade-top");
+
+const fadersTopOptions = {
+    threshold: 0.3,
+};
+
+const fadersTopObserver = new IntersectionObserver(
+    (entries, fadersTopObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear-top");
+            setTimeout(() => {
+                entry.target.style.cssText = `transition-delay = 0`
+            }, 500);
+        }
+    });
+    },
+    fadersTopOptions
+);
+
+fadersTop.forEach((fader) => {
+    fadersTopObserver.observe(fader);
+});
+    
+    // ========================== Fade down ==========================
+const fadersDown = document.querySelectorAll(".fade-down");
+
+const fadersDownOptions = {
+    threshold: 0.3,
+};
+    
+const fadersDownObserver = new IntersectionObserver(
+    (entries, fadersDownObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear-down");
+            setTimeout(() => {
+                entry.target.style.cssText = `transition-delay = 0`
+            }, 500);
+        }
+    });
+    },
+    fadersDownOptions
+);
+
+fadersDown.forEach((fader) => {
+    fadersDownObserver.observe(fader);
+});
+
+    // ========================== Fade left ==========================
+const fadersLeft = document.querySelectorAll(".fade-left");
+
+const fadersLeftOptions = {
+    threshold: 0.5,
+};
+    
+const fadersLeftObserver = new IntersectionObserver(
+    (entries, fadersLeftObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear-left");
+            setTimeout(() => {
+                entry.target.style.cssText = `transition-delay = 0`
+            }, 500);
+        }
+    });
+    },
+    fadersLeftOptions
+);
+
+fadersLeft.forEach((fader) => {
+    fadersLeftObserver.observe(fader);
+});
+
+    // ========================== Fade right ==========================
+const fadersRight = document.querySelectorAll(".fade-right");
+
+const fadersRightOptions = {
+    threshold: 0.5,
+};
+    
+const fadersRightObserver = new IntersectionObserver(
+    (entries, fadersRightObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear-right");
+            setTimeout(() => {
+                entry.target.style.cssText = `transition-delay = 0`
+            }, 500);
+        }
+    });
+    },
+    fadersRightOptions
+);
+
+fadersRight.forEach((fader) => {
+    fadersRightObserver.observe(fader);
+});
+});
